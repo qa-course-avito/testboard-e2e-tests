2dda93f00dea1c28570f033695c8997c3da60f16
dvborschevskiy <dvborschevskiy@avito.ru>
2026-03-03 14:00:12 +0300

    e2e-tests-update

--- tests/accountTests/openLoginForm.test.ts
+++ tests/accountTests/openLoginForm.test.ts
@@ -3,26 +3,6 @@ import {LoginPopupPage} from "../../pages/loginPopupPage/loginPopupPage";
 import {MainPage} from "../../pages/mainPage/mainPage";
 
 test.describe("Проверки попапа с авторизацией", () => {
-    test("Успешная авторизация", async ({ page }) => {
-        //arrange
-        const loginPopupPage = new LoginPopupPage(page);
-        const mainPage = new MainPage(page);
-
-        const uniq = Date.now();
-        const email = `gedeon.qa+${uniq}@example.ru`;
-        const password = "Password123";
-
-        //act
-        await mainPage.openMainPage();
-        await mainPage.openLoginDesktop();
-        await loginPopupPage.fillLogin(email);
-        await loginPopupPage.fillPassword(password);
-        await loginPopupPage.clickLoginBtn();
-
-        //assert
-        await mainPage.assertUserIsLoggedIn();
-    });
-
     test("переход на регистрацию по кнопке", async ({ page }) => {
         //arrange
         const loginPopup = new LoginPopupPage(page);
@@ -34,7 +14,7 @@ test.describe("Проверки попапа с авторизацией", () =>
         await loginPopup.clickRegisterBtn();
 
         //assert
-        await expect(page).toHaveURL("/authRegistration");
+        await expect(page).toHaveURL("auth/register");
     });
 
     test("логин с пустыми полями не должен увести на главную", async ({ page }) => {
